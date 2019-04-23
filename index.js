91f18f30911f2720ae209f3a1f63a194d18f4f0f
Ian Springer <ian.springer@bvaccel.com>
2019-04-22 22:05:14 -0700

    saving

--- index.js
+++ index.js
@@ -23,9 +23,3 @@ app.use('/', routes)
 
 app.listen(3000, () => console.log("Listening on port 3000"));
 
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
